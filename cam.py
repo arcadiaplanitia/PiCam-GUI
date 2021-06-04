1751a8c766fa8d6e3c812bec5a458cf92b246b07
arcadiaplanitia <69369213+arcadiaplanitia@users.noreply.github.com>
2021-06-03 22:32:51 -0400

    Improved buttons - cam.py
    Improved buttons, now you can adjust padding with the variable
    
    Need to work out system for multiple buttons, and each causing different things to happen.

--- cam.py
+++ cam.py
@@ -23,10 +23,16 @@ font = pygame.font.Font(None, 24) #Create font
 #Create some colors
 black = 0,0,0
 white = 255,255,255
+red = 255,0,0
+blue = 0,0,255
+
+layer = pygame.Surface([width, height], pygame.SRCALPHA) #Creates transparent surface for GUI
+padding = 40
+clicked = False
 
 #Functions
 class Button:
-    def __init__(self, text,  pos, font, bg, feedback=""):
+    def __init__(self, text,  pos, font, bg="black", feedback=""):
         self.x, self.y = pos
         self.font = pygame.font.SysFont("Arial", font)
         if feedback == "":
@@ -35,42 +41,43 @@ class Button:
             self.feedback = feedback
         self.change_text(text, bg)
 
-    def change_text(self, text, bg):
-        self.text = self.font.render(text, 1, pygame.Color("White"))
+    def change_text(self, text, bg="black"):
+        self.text = self.font.render(text, 1, pygame.Color("Black"))
         self.size = self.text.get_size()
-        self.surface = pygame.Surface(self.size)
+        self.surface = pygame.Surface((self.size[0]+padding, self.size[1]+padding))
         self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
-        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+        self.surface.blit(self.text, (0+(padding/2), 0+(padding/2)))
+        self.rect = pygame.Rect(self.x - padding, self.y - padding, self.size[0] + padding, self.size[1] + padding)
 
     def show(self):
         layer.blit(button1.surface, (self.x, self.y))
 
     def click(self, event):
+        global clicked 
         x, y = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
-                if self.rect.collidepoint(x, y):
-                    self.change_text(self.feedback, bg=white)
+                if self.rect.collidepoint(x - padding, y - padding):
+                    if not clicked:
+                        self.change_text(self.feedback, bg=red)
+                        clicked = True
+                    elif clicked:
+                        self.change_text(self.feedback, bg=blue)
+                        clicked = False
 
 
-
-layer = pygame.Surface([width, height], pygame.SRCALPHA) #Creates transparent surface for GUI
-text = 'Hello'
-textGraphic = font.render(text, True, white)
 
 camera = picamera.PiCamera() #Setup camera
 camera.resolution = (4056, 2434) #Set resolution
 camera.start_preview() #Start preview
 
 #Buttons
-button1 = Button("Click here", (100, 100), font=30, bg=black, feedback="You clicked me")
+button1 = Button("Click here", (100, 100), font=30, bg=black, feedback="Clicked")
 
 # Main loop
 while alive:
     screen.fill((0, 0, 0))
     layer.fill((0, 0, 0, 0))
-    layer.blit(textGraphic, (100,100))
 
     for event in pygame.event.get(): #Detects key press (temp until GUI)
         if event.type == KEYDOWN:
@@ -79,9 +86,14 @@ while alive:
                 pygame.quit()
                 camera.stop_preview()
                 raise SystemExit
+        if event.type == pygame.QUIT:
+            alive = False
+            pygame.quit()
+            raise SystemExit
         button1.click(event)
     
     button1.show()
+
     pygamesScreenRaw = pygame.image.tostring(layer, 'RGBA')
     if firstLoop:
         o = camera.add_overlay(pygamesScreenRaw, size=(width, height), fullscreen=True)
@@ -95,3 +107,4 @@ while alive:
     
 
     clock.tick(60)
+
